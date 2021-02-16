8e01e902959872d0a18542a16829272647f035b2
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2021-02-16 19:15:38 +0100

    styling home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="home">
-    <div style="height: 80vh">
-      Home
+    <div class="row container banner">
+      <div class="d-none d-sm-block col-lg-7 banner-image">
+        <img src="../assets/banner.jpg">
+      </div>
+      <div class="col-12 col-lg-5 banner-info">
+        <h1>Real estate is sold with McMakler </h1>
+      </div>
     </div>
   </div>
 </template>
@@ -12,5 +17,32 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+img {
+  width: 100%;
+}
+h1 {
+  position: relative;
+  height: max-content;
+  white-space: nowrap;
+  top: 40px;
+  right: 100px;
+  background-color: white;
+  padding: 36px 52px;
+}
+.banner-info{
+  height: 70vh;
+}
+.search {
+  position: absolute;
+  bottom: 0;
+}
+
+@media (max-width: 767px){
+  h1 {
+    white-space: normal;
+    position: unset;
+    padding: 0;
+  }
+}
 </style>
