3db1db154bf904671e53132d60ab4f2cabdd1dad
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2021-02-18 13:07:03 +0100

    merge

--- src/components/MyApartments.vue
+++ src/components/MyApartments.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="my-apartments">
-    My Apartments
+
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'MyApartments'
+  name: 'MyApartments',
+  data () {
+    return {
+      properties: {}
+    }
+  },
+  created () {
+    axios.get('http://localhost:3000/api/v1/my_properties')
+      .then(response => {
+        console.log(response)
+        // this.properties = response.data.properties
+      })
+      .catch(error => console.log(error))
+    // console.log(this.properties)
+  }
 }
 </script>
 
