8ad639e7c0c7fd7f3082bf2ea36ee65640ba9e96
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2021-02-19 20:41:36 +0100

    nav bar responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,51 +1,59 @@
 <template>
   <div id="navbar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light flex-column">
-      <div class="w-100 d-flex justify-content-center mt-2">
-        <!-- LEFT -->
-        <router-link to="/">
-          <h1 style="color: #2c3e52">McMackler</h1>
-        </router-link>
-        <!-- CENTER -->
-        <div class="collapse navbar-collapse justify-content-center align-items-center" id="navbarSupportedContent">
-          <ul class="navbar-nav align-items-center">
-            <div class="d-flex align-items-center">
-              <router-link to="/apartments" class="nav-link">
-                <li class="nav-item">
-                  <p class="mb-0">Appartments</p>
-                </li>
-              </router-link>
-              <router-link to="/" class="nav-link">
-                <li class="nav-item">
-                  <p class="mb-0">What's New</p>
-                </li>
-              </router-link>
-              <router-link to="/" class="nav-link">
-                <li class="nav-item">Premium Appartments</li>
-              </router-link>
-              <router-link to="/myappartments" class="nav-link" v-if="loggedIn()">
-                <li class="nav-item">My Properties</li>
-              </router-link>
-            </div>
-            <li><router-link  active-class="active"
-              :to="{ name: 'Add' }"
-              v-if="loggedIn()">
-              <a href="#" class="nav-link btn-yellow">Add Property</a>
-            </router-link></li>
+    <div>
+      <b-navbar toggleable="sm" type="light" variant="light">
+        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+        <!-- <div class="w-100 d-flex justify-content-center mt-2"> -->
+          <!-- Title Left -->
+          <b-navbar-brand>
+            <router-link to="/">
+              <h1 style="color: #2c3e52">McMackler</h1>
+            </router-link>
+          </b-navbar-brand>
+          <!-- Links Center -->
+          <b-collapse id="nav-text-collapse" is-nav>
+            <b-navbar-nav class="d-flex align-items-center justify-content-center w-100">
+              <div class="flex-grow-1">
+                <b-nav-text>
+                  <router-link to="/apartments" class="nav-link">
+                    <p class="mb-0">Appartments</p>
+                  </router-link>
+                </b-nav-text>
+                <b-nav-text>
+                  <router-link to="/" class="nav-link">
+                    <p class="mb-0">What's New</p>
+                  </router-link>
+                </b-nav-text>
+                <b-nav-text>
+                  <router-link to="/" class="nav-link">
+                    <p class="mb-0">Premium Appartments</p>
+                  </router-link>
+                </b-nav-text>
+                <b-nav-text>
+                  <router-link  active-class="active"
+                    :to="{ name: 'Add' }"
+                    v-if="loggedIn()">
+                    <a href="#" class="nav-link btn-yellow">Add Property</a>
+                  </router-link>
+                </b-nav-text>
+                <b-nav-text>
+                  <router-link to="/myappartments" class="nav-link" v-if="loggedIn()">
+                    <p class="mb-0">My Properties</p>
+                  </router-link>
+                </b-nav-text>
+              </div>
 
-            <!-- <router-link :to="{ name: 'add' }" class="nav-link" v-if="loggedIn()">
-              <li class="nav-item btn-yellow"><a href="#" class="nav-link">Add Apartment</a></li>
-            </router-link> -->
-          </ul>
-        </div>
-        <!-- RIGHT -->
-        <div class="d-flex justify-content-end align-items-center pr-5">
-          <!-- eslint-disable-next-line -->
-          <router-link to="/signin" class="nav-link btn nav-link" v-if="!loggedIn()">Sign In</router-link>
-          <button to="/" class="btn nav-link" @click.prevent="logout()" v-if="loggedIn()">Log Out</button>
-        </div>
-      </div>
-    </nav>
+              <b-nav-text>
+                <router-link to="/signin" class="nav-link btn nav-link" v-if="!loggedIn()">Sign In</router-link>
+              </b-nav-text>
+              <b-nav-text>
+                <button to="/" class="btn nav-link" @click.prevent="logout()" v-if="loggedIn()">Log Out</button>
+              </b-nav-text>
+            </b-navbar-nav>
+          </b-collapse>
+        <!-- </div> -->
+      </b-navbar>
+    </div>
   </div>
 </template>
 
@@ -80,16 +88,19 @@ export default {
 </script>
 
 <style scoped>
-.btn-yellow {
-  background-color: #ffe300;
-  padding: 4px 12px;
-  border-radius: 4px;
-}
-a:hover {
-  text-decoration: none;
-  color: #39393a;
-}
-h1 {
-  font-weight: 700;
-}
+  .btn-yellow {
+    background-color: #ffe300;
+    padding: 4px 12px;
+    border-radius: 4px;
+  }
+  a:hover {
+    text-decoration: none;
+    color: #39393a;
+  }
+  h1 {
+    font-weight: 700;
+  }
+  .navbar-expand-lg .navbar-nav {
+    flex-direction: column-reverse !important;
+  }
 </style>
